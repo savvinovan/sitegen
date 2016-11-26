ac182a6c2eb043f8836a86f537518a6d46a6c8c4
Alexandr Savvinov <savvinovan@gmail.com>
2016-11-26 16:49:47 +0900

    wikipedia done

--- application/controllers/get_wiki.php
+++ application/controllers/get_wiki.php
@@ -1,4 +1,7 @@
 <?php
+require '../vendor/autoload.php';
+use Webmozart\Json\JsonDecoder;
+$decoder = new JsonDecoder();
 
 class GetWiki {
   public $req;
@@ -6,17 +9,20 @@ class GetWiki {
   function __construct($req) {
     $this->req = $req;
   }
-  function getReq() {
+  function get_req() {
     return $this->req;
   }
-  function getWiki() {
-    return 'yoyo';
-  }
-  function requestWiki() {
-    $this->response = file_get_contents("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" . $this->req);
+  function request_wiki() {
+    $this->response = file_get_contents("https://ru.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&exintro=&explaintext=&titles=" . $this->req);
   }
   function parseResponse() {
-    
+    $this->request_wiki();
+    $data = simplexml_load_string($this->response);
+    //return current($data->query->pages)->extract;
+    return $data->query->pages->page->extract->{0}->__toString();
+  }
+  function get_first_element() {
+
   }
 }
 
--- www/index.php
+++ www/index.php
@@ -11,13 +11,13 @@ if ($req[0] == 'api') {
     // News Class пример класса ньюс
     require '../application/controllers/get_news.php';
     $gn = new GetNews($req[2]);
-    print_r($gn->getReq());
+    //print_r($gn->getReq());
     // Wiki class
     require '../application/controllers/get_wiki.php';
     $gw = new GetWiki($req[2]);
-    print_r($gw->getWiki());
+    $ex = $gw->parseResponse();
+    echo $ex;
     // Search Image class
-
     // MySQL store
 
     // ============ Generate Template ==========
