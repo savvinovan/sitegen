31f7ac6bcdb1a965fd1dae7f6271a262d8c6c121
Dorofei <ndorofei@gmail.com>
2016-11-26 20:54:11 +0900

    исправил кодировку

--- application/controllers/get_news.php
+++ application/controllers/get_news.php
@@ -23,11 +23,10 @@ class GetNews {
   	    echo 'Link: ', $item->link.'<br>';
   	    echo 'Timestamp: ', $item->timestamp.'<br>';
   	    echo 'Description ', $item->description.'<br>';
-$saw = new nokogiri(iconv('windows-1251','UTF-8',$item->description));
+$saw = nokogiri::fromHtmlNoCharset($item->description);
 $arr=$saw->get('font[size=-1]')->toArray();
-
-  	    //echo 'Description очищенный: ', iconv('KOER','UTF-8',$arr[1]['#text'][0]);
-echo 'Description очищенный: ', $arr[1]['#text'][0];
+echo 'Description очищенный Картинка: ', $arr[1]['#text'][0].'<br>';
+echo 'Description очищенный Описание: ', $arr[1]['#text'][0].'<br>';
   	    echo 'HTML encoded content: ', $item->{'content:encoded'}.'<br><br>';
   	}
       return $feed;
