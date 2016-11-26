791130fa82cc4bac15011dea850a679fc2025d5d
ptflp <globallinkliberty@yandex.ru>
2016-11-26 16:09:19 +0900

    SQL manager
    create.php создает базу
    update.php обновляет
    backup.php создает бэкап

---
+++ www/sql/create.sql
@@ -0,0 +1,33 @@
+
+
+CREATE TABLE `img` (
+  `id` int(11) NOT NULL,
+  `requestID` int(11) NOT NULL,
+  `url` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `requestID` int(11) NOT NULL,
+  `text` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+
+
+CREATE TABLE `wiki` (
+  `id` int(11) NOT NULL,
+  `requestID` int(11) NOT NULL,
+  `text` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
