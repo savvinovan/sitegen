4f3a6d08e306845dcf5f8f1247ae7cdaf318b134
ptflp <globallinkliberty@yandex.ru>
2016-11-27 04:02:37 +0900

    супер комит

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
 $req = explode('/', $_REQUEST['path']);
 if ($req[0] == 'api') {
   // req = api
-  if ($req[1] == 'v1' && strlen($req[2])>1) {
+  if ($req[1] == 'v1' && strlen($req[2])>1 ) {
     spl_autoload_register(function ($class_name) {
         include '../application/controllers/'.$class_name . '.php';
     });
@@ -13,6 +13,7 @@ if ($req[0] == 'api') {
     // ========= Get Infos ========
     // News Class пример класса ньюс    
     $gn = new get_news($req[2]);
+    $newsArr = (array) $gn->getReq();
     //print_r($gn->getReq());
     // Wiki class    
     $gw = new get_wiki($req[2]);
@@ -21,14 +22,15 @@ if ($req[0] == 'api') {
     $translate = new translate($req[2]);
     $req['en']=$translate->en;
     // Search Image class    
-    $img = new get_img($req['en']);   
-    print_r($img->urls[0]);
+    $img = new get_img($req['en']);   //thumbnail, small, regular, full, raw $img->urls[0]['raw']
     // Youtube search api    
-    $gy = new GetYoutube($req[2]);
+    $gy = new get_youtube($req[2]);
     $data = $gy->getVideoUrl();
     $data[0]->id->videoId; 
 
     // ============ Generate Template ==========
+    $view = new view($req[4]);
+    include ($view->dir);
   }
   else {
     echo 'version error';
