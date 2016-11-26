11e4acd4dbda3ac2899a1db726206e59ffdbfe2f
ptflp <globallinkliberty@yandex.ru>
2016-11-27 00:39:37 +0900

    added Model module get_img
    xD

---
+++ application/controllers/get_img.php
@@ -0,0 +1,10 @@
+<?
+class getImg {
+	private $urls;
+    public function getJSON ($req) {
+        $data=file_get_contents('https://api.unsplash.com/search/photos?page=1&query='.$req.'&client_id=8cc66c0a3a8f651fec9ad092868115b9b55843b113e78200ed3c94768af95b1a');
+        $json=json_decode($data,true);
+        $this->urls=$json['results'];
+        return $json['results'];
+    }
+}
