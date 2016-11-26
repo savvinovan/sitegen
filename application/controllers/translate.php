b775aaf7cbc2656d404f1f9afc57fafa8b22996e
ptflp <globallinkliberty@yandex.ru>
2016-11-27 00:52:24 +0900

    minor fix

--- application/controllers/translate.php
+++ application/controllers/translate.php
@@ -1,8 +1,10 @@
 <?
 class ytranslate {
 	private $ru;
+    public function rutran ($req) {
         $data=file_get_contents('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161126T153744Z.f9875c0a9f6d2f0b.53742959965ce159ceacfce62767b60365b6aa85&text='.$req.'&lang=ru-en');
         $json=json_decode($data,true);
         $this->ru=$json['text'][0];
+        return $this->ru;
     }
 }
