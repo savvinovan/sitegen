e4966ab35cb8c37f6279c879a311c5310fbecdb9
ptflp <globallinkliberty@yandex.ru>
2016-11-27 01:06:35 +0900

    minor fixes

--- application/controllers/get_img.php
+++ application/controllers/get_img.php
@@ -1,6 +1,9 @@
 <?
 class getImg {
 	private $urls;
+	function __construct($req) {
+		$this->getJSON($req);
+	}
     public function getJSON ($req) {
         $data=file_get_contents('https://api.unsplash.com/search/photos?page=1&query='.$req.'&client_id=8cc66c0a3a8f651fec9ad092868115b9b55843b113e78200ed3c94768af95b1a');
         $json=json_decode($data,true);
--- application/controllers/translate.php
+++ application/controllers/translate.php
@@ -1,10 +1,13 @@
 <?
 class ytranslate {
-	private $ru;
+	private $en;
+	function __construct ($req) {
+		$this->rutran($req);
+	}
     public function rutran ($req) {
         $data=file_get_contents('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161126T153744Z.f9875c0a9f6d2f0b.53742959965ce159ceacfce62767b60365b6aa85&text='.$req.'&lang=ru-en');
         $json=json_decode($data,true);
-        $this->ru=$json['text'][0];
-        return $this->ru;
+        $this->en=$json['text'][0];
+        return $this->en;
     }
 }
