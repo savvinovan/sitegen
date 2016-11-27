5f2c5352a09daef80471873cf1ad473fa96e3fe6
Dorofei <ndorofei@gmail.com>
2016-11-27 15:20:27 +0900

    Добавил сохранение шаблонов
    и добавил gitignore

--- www/index.php
+++ www/index.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 // Requets Router
 // Для теста вашего
 $req = explode('/', $_REQUEST['path']);
@@ -10,6 +11,11 @@ if ($req[0] == 'api') {
     });
     // req = api/v1
     // $req[2] - слово через которое мы генерируем сайт
+
+    // ========= Chech query ========
+
+    
+
 
     // ========= Get Infos ========
 
@@ -28,7 +34,7 @@ if ($req[0] == 'api') {
     $req['en']=$translate->en;
     
     // Search Image class    
-    $img = new get_img($req['en']);   //thumbnail, small, regular, full, raw $img->urls[0]['raw']
+    //$img = new get_img($req['en']);   //thumbnail, small, regular, full, raw $img->urls[0]['raw']
     
     // Youtube search api    
     $gy = new get_youtube($req[2]);
@@ -36,15 +42,34 @@ if ($req[0] == 'api') {
     $data[0]->id->videoId; 
 
     // ============ Generate Template ==========
+    // ============ and save file     ==========
     $view = new view($req[4]);
-    include ($view->dir);
+    include($view->dir);
   }
   else {
     echo 'version error';
   }
-} else
-{
+} elseif ($req[0] == 'get-site') {
+    if (file_exists('../sites/'.$req[1].'/dump.html')) 
+    {
+        include('../sites/'.$req[1].'/dump.html');
+    }
+} elseif ($req[0] == 'gen-site') {
+    // ============ Generate Template ==========
+    // ============ and save file     ==========
+    //$hash_query = hash('md5',$req[1]); //в таком случае для одного слова контент будет постоянным
+    $hash_query = hash('md5',uniqid());
+
+    if (!file_exists('../sites/'.$hash_query)){
+        mkdir('../sites/'.$hash_query);
+    }
+    $dump_site = file_get_contents('http://sitegen/api/v1/'.$req[1]);
+    $filename = '../sites/'.$hash_query.'/dump.html';
+    file_put_contents($filename, $dump_site);
+    echo $hash_query;
+} else{
   echo 'error';
+  print_r($req);
 }
 
 ?>
