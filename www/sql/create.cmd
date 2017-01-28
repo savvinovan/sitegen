9b04a3f96326d61359b60747171e09607d54c498
ptflp <globallinkliberty@yandex.ru>
2017-01-29 03:05:03 +0900

    create cmd optimized

--- www/sql/create.cmd
+++ www/sql/create.cmd
@@ -1,3 +1,2 @@
 xcopy /Y "Z:\home\sitegen\www\sql\create.sql" "Z:\usr\local\mysql-5.5\bin\"
-cd "Z:\usr\local\mysql-5.5\bin\"
-mysql -u root sitegen < create.sql
+"Z:\usr\local\mysql-5.5\bin\mysql" -u root sitegen < create.sql
